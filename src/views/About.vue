19d56448559642f543722ff120fa30cf990027ea
yangguanghe <1649063797@qq.com>
2021-02-03 01:03:55 +0800

    修复在pc端样式重叠的问题

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <el-row :gutter="20">
+    <el-row :gutter="30">
       <el-col :xs="24" :sm="11" :md="8" :lg="7" :xl="7">
         <div class="left">
           <el-card class="box-card">
@@ -46,57 +46,17 @@
           <el-card class="box-card">
             <el-tabs v-model="activeName" @tab-click="handleClick">
               <el-tab-pane label="最新文章" name="first">
-                <div class="block">
-                  <el-timeline>
+                  <el-timeline :reverse="true">
                     <el-timeline-item v-for="(item, index) in blogList" :key="index" :timestamp="item.createTime | timeStr" placement="top">
                       <el-card>
-                        <h4 style="padding-bottom: 12px">{{ item.title }}</h4>
+                        <h4 style="padding-bottom: 0.75rem">{{ item.title }}</h4>
                         <p>杨光贺 发布于 {{ item.createTime | timeStr }}</p>
                       </el-card>
                     </el-timeline-item>
                   </el-timeline>
-                </div>
               </el-tab-pane>
               <el-tab-pane label="配置" name="second">
-                <el-form ref="form" :model="form" label-width="80px">
-                  <el-form-item label="活动名称">
-                    <el-input v-model="form.name"></el-input>
-                  </el-form-item>
-                  <el-form-item label="活动区域">
-                    <el-select v-model="form.region" placeholder="请选择活动区域">
-                      <el-option label="区域一" value="shanghai"></el-option>
-                      <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                  </el-form-item>
-                  <el-form-item label="活动时间">
-                    <el-col :span="11">
-                      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-                    </el-col>
-                    <el-col class="line" :span="2">-</el-col>
-                    <el-col :span="11">
-                      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-                    </el-col>
-                  </el-form-item>
-                  <el-form-item label="即时配送">
-                    <el-switch v-model="form.delivery"></el-switch>
-                  </el-form-item>
-                  <el-form-item label="活动性质">
-                    <el-checkbox-group v-model="form.type">
-                      <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                      <el-checkbox label="地推活动" name="type"></el-checkbox>
-                      <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                      <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                    </el-checkbox-group>
-                  </el-form-item>
-                  <el-form-item label="特殊资源">
-                    <el-radio-group v-model="form.resource">
-                      <el-radio label="线上品牌商赞助"></el-radio>
-                      <el-radio label="线下场地免费"></el-radio>
-                    </el-radio-group>
-                  </el-form-item>
-                  <el-form-item label="活动形式">
-                    <el-input type="textarea" v-model="form.desc"></el-input>
-                  </el-form-item>
+                <el-form ref="form" :model="form" label-width="5rem">
                   <el-form-item>
                     <el-button type="primary" @click="onSubmit">立即创建</el-button>
                     <el-button>取消</el-button>
@@ -206,7 +166,6 @@ export default {
     max-width: 80rem;
     margin: 0 auto;
     .left {
-      max-width: 32rem;
       margin-bottom: 1.75rem;
       .box-card {
         width: 100%;
@@ -228,6 +187,7 @@ export default {
           }
         }
         .info {
+          flex: 1;
           padding-left: 1.625rem;
           display: flex;
           flex-wrap: wrap;
@@ -252,13 +212,17 @@ export default {
           }
           .button-new-tag {
             height: 2rem;
+            width: 5.5rem;
             line-height: 1.875rem;
             padding-top: 0;
             padding-bottom: 0;
           }
           .input-new-tag {
-            width: 5rem;
-            margin-left: 0.625rem;
+            width: 5.5rem;
+            padding-top: 0;
+            padding-bottom: 0;
+            /*margin-left: 0.625rem;*/
+            margin-bottom: 0.625rem;
             vertical-align: bottom;
           }
         }

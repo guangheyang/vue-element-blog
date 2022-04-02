aea5f41e3535c861a372a0a14e7722b4df45326d
yangguanghe <1649063797@qq.com>
2022-04-02 17:50:52 +0800

    使用mavon-editor代替富文本编译器

---
+++ src/components/EditorComp/Editor.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="editor">
+    <mavon-editor
+      style="height: 100%"
+      :subfield="false"
+      :value="value"
+      :toolbars="toolbars"
+      @change="change"
+      @imgAdd="imgAdd"
+      @imgDel="imgDel"
+    />
+  </div>
+</template>
+<script>
+// Local Registration
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+export default {
+  name: "editor",
+  components: {
+    mavonEditor,
+  },
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      toolbars: {
+        italic: true, // 斜体
+        header: true, // 标题
+        underline: true, // 下划线
+        strikethrough: true, // 中划线
+        mark: true, // 标记
+        superscript: true, // 上角标
+        subscript: true, // 下角标
+        quote: true, // 引用
+        ol: true, // 有序列表
+        ul: true, // 无序列表
+        link: true, // 链接
+        imagelink: true, // 图片链接
+        code: true, // code
+        table: true, // 表格
+        fullscreen: true, // 全屏编辑
+        readmodel: true, // 沉浸式阅读
+        rash: true, // 清空
+        save: false, // 保存（触发events中的save事件）
+        alignleft: true, // 左对齐
+        aligncenter: true, // 居中
+        alignright: true, // 右对齐
+      }
+    };
+  },
+  methods: {
+    change(value, render) {
+      this.$emit('change-value', render, value)
+    },
+    imgAdd(pos, $file) {
+      this.$emit('img-add', pos, $file)
+    },
+    imgDel(fileName) {
+      this.$emit('img-add', fileName)
+    }
+  },
+};
+</script>
--- src/views/EditBlog.vue
+++ src/views/EditBlog.vue
@@ -1,23 +1,143 @@
 <template>
   <div class="editBlog">
     <el-card class="box-card">
-      <Editor></Editor>
+      <div class="editor-container">
+        <el-form ref="form" :model="form" :rules="rules">
+          <el-form-item label="标题" prop="title">
+            <el-input
+              v-model="form.title"
+              show-word-limit
+              maxlength="20"
+              style="width: 226px"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="module">
+            <el-select v-model="form.module">
+              <el-option label="全部可见" value="1" />
+              <el-option label="仅自己可见" value="2" />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="内容" prop="content" class="vab-quill-content">
+            <Editor :value="value" @change-value="changeValue" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="handleSee">预览效果</el-button>
+            <el-button type="primary" :loading="isClickSave" @click="handleSave">保存</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </el-card>
+    <el-dialog title="博客预览效果" width="80%" :visible.sync="dialogTableVisible">
+      <div class="preview">
+        <h1 class="news-title">{{ form.title }}</h1>
+        <div class="news-content" v-html="form.content"></div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import Editor from '@/components/Editor'
+import Editor from "@/components/EditorComp/Editor";
+import { insertBlog } from '@/api/editor'
+import { message } from "@/utils/message";
 export default {
   components: {
-    Editor
-  }
-}
+    Editor,
+  },
+  data() {
+    return {
+      form: {
+        title: "",
+        module: "",
+        content: "",
+      },
+      value: '',
+      isClickSave: false,
+      dialogTableVisible: false,
+      rules: {
+        title: [
+          {
+            required: true,
+            message: "请输入标题",
+            trigger: "blur",
+          },
+        ],
+        module: [
+          {
+            required: true,
+            message: "请选择模块",
+            trigger: "change",
+          },
+        ],
+        content: [
+          {
+            required: true,
+            message: "请输入内容",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  methods: {
+    handleSave() {
+      this.$refs["form"].validate((valid) => {
+        this.isClickSave = true;
+        if (valid) {
+          const { title, content: data } = this.form;
+          insertBlog({ title, data }).then((res) => {
+            if (res.code === 20000) {
+              message({ message: "操作成功", type: "success" });
+              this.$router.push({ path: "/" });
+            }
+          });
+        } else {
+          this.isClickSave = false;
+          return false;
+        }
+      });
+    },
+    handleSee(valid) {
+      if (valid) {
+        this.dialogTableVisible = true;
+      } else {
+        return false;
+      }
+    },
+     changeValue(render) {
+      this.form.content = render
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .editBlog {
-    width: 1180px;
-    margin: 0 auto;
+.editBlog {
+  margin: 0 1rem;
+}
+.box-card {
+  max-width: 80rem;
+  margin: 0 auto;
+}
+</style>
+<style lang="scss">
+.preview {
+  min-height: 60vh;
+  .news-title {
+    color: #2e2e2e;
+    font-size: 3rem;
+    font-weight: 700;
+    font-weight: bolder;
+    padding: 10px 0 20px;
   }
+  .news-content {
+    margin: 5rem 0;
+    font-size: 1.2rem;
+    text-indent: 2em;
+    img {
+      max-width: 37.5rem;
+      margin-bottom: 0.625px;
+    }
+  }
+}
 </style>
